d3d569f42a1ee108df69dd4f68052f017dee6c44
okoliken <jahbuchimokoli3@gmail.com>
2024-08-03 14:20:47 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors:{
+        pvGreen: "#B9FF66",
+        pvGrey:"#F3F3F3",
+        pvDark:"#191A23",
+        pvLightGrey:"#292A32"
+      },
+      container:{
+        center:true
+      }
+    },
+  },
+  plugins: [],
+}
+
